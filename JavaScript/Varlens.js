3c7bc7dee9e6f437d8b65c00629da484eca76043
alodaner <alodaner@gmail.com>
2022-02-23 12:31:06 +0800

    Update Varlens.js

--- JavaScript/Varlens.js
+++ JavaScript/Varlens.js
@@ -1,4 +1,38 @@
-var ddgksf2013 = 
-{"success":true,"data":{"username":"Sthion","app_name":"Sthion","nick":null,"email":"","status":0,"preferences":null,"membership":{"role_name":"pro","begin_time":"2021-03-13T09:59:11.485Z","end_time":"2099-12-31T00:00:00.481Z","begin_time_ms":1615629551485,"end_time_ms":4102325303000},"social_apple_id":null}};
+/*
+ioshkj
 
-$done({body : JSON.stringify(ddgksf2013)});
+QX:
+[rewrite_local]
+#Varlens解锁PRO
+^https:\/\/mid\.zineapi\.com\/@varlens\/api\/user\/info\/ url script-response-body vspro.js
+
+[mitm]
+hostname = mid.zineapi.com
+
+下载地址；
+https://apps.apple.com/cn/app/id1535205062
+
+*/
+
+let obj = JSON.parse($response.body);
+obj = {
+  "success": true,
+  "data": {
+    "username": "㍼㍿",
+    "app_name": "varlens",
+    "nick": null,
+    "email": "",
+    "status": 0,
+    "preferences": null,
+    "membership": {
+      "role_name": "pro",
+      "begin_time": "2021-03-13T09:59:11.485Z",
+      "end_time": "2045-12- 31T00:00:00.481Z",
+      "begin_time_ms": 1615629551485,
+      "end_time_ms": 2398327914000
+    },
+    "social_apple_id": null
+  }
+}
+
+$done({body: JSON.stringify(obj)});
